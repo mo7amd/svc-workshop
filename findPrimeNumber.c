6d70761ebf1261615934cc6d5719c7a6be4e40b0
mohamed khaled khalil <mo7amd.khaled@gmail.com>
2018-01-27 17:08:59 +0200

    add isPrimenumber function

--- findPrimeNumber.c
+++ findPrimeNumber.c
@@ -1,3 +1,26 @@
 /* 
 * create function to find prime number
 */
+
+#include <stdio.h>
+
+int isPrimeNumber( int number );
+
+int main () {
+    isPrimeNumber(3);
+}
+
+int isPrimeNumber( int number ) { 
+
+    int isPrimeNumber = 1;
+
+    for ( int divider = 1; divider <= number/2; divider++ ) {
+
+        if ( number % divider == 0 ) {
+            isPrimeNumber = 0;
+        }
+
+    }
+    isPrimeNumber ? printf("%d is prime number \n", number) : printf("%d is not prime number \n", number);
+    return isPrimeNumber;
+}
